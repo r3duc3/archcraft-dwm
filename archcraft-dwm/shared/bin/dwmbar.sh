00048e81211725326931a8485374092ac15ba34a
r3duc3 <itsreduce@gmail.com>
2022-12-22 21:27:56 +0700

    custom bar

--- archcraft-dwm/shared/bin/dwmbar.sh
+++ archcraft-dwm/shared/bin/dwmbar.sh
@@ -2,42 +2,20 @@
 
 interval=0
 
-## Cpu Info
-cpu_info() {
-	cpu_load=$(grep -o "^[^ ]*" /proc/loadavg)
-
-	printf "^c#3b414d^ ^b#7ec7a2^ CPU"
-	printf "^c#abb2bf^ ^b#353b45^ $cpu_load"
-}
-
-## Memory
-memory() {
-	printf "^c#C678DD^^b#1e222a^   $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g) "
-}
-
 ## Wi-fi
 wlan() {
 	case "$(cat /sys/class/net/w*/operstate 2>/dev/null)" in
-		up) printf "^c#3b414d^^b#7aa2f7^  ^d^%s" " ^c#7aa2f7^Connected " ;;
-		down) printf "^c#3b414d^^b#E06C75^ 睊 ^d^%s" " ^c#E06C75^Disconnected " ;;
+		up)
+			CONNAME=$(nmcli -t -f STATE,CONNECTION device status | grep '^connected:' | sed 's/connected\://')
+			printf "^c#3b414d^^b#7aa2f7^  ^d^%s" " ^c#7aa2f7^$CONNAME " ;;
+		down) printf "^c#3b414d^^b#E06C75^ 睊 ^d^%s" " ^c#E06C75^Dc " ;;
 	esac
 }
 
 ## Time
 clock() {
 	printf "^c#1e222a^^b#668ee3^  "
-	printf "^c#1e222a^^b#7aa2f7^ $(date '+%a, %I:%M %p') "
-}
-
-## System Update
-updates() {
-	updates=$(checkupdates | wc -l)
-
-	if [ -z "$updates" ]; then
-		printf "^c#98C379^  Updated"
-	else
-		printf "^c#98C379^  $updates"" updates"
-	fi
+	printf "^c#1e222a^^b#7aa2f7^ $(date '+%a, %d-%m-%y %H:%M:%S') "
 }
 
 ## Battery Info
@@ -64,25 +42,7 @@ battery() {
 	fi
 }
 
-## Brightness
-brightness() {
-	LIGHT=$(printf "%.0f\n" `light -G`)
-
-	if [[ ("$LIGHT" -ge "0") && ("$LIGHT" -le "25") ]]; then
-		printf "^c#56B6C2^  $LIGHT%%"
-	elif [[ ("$LIGHT" -ge "25") && ("$LIGHT" -le "50") ]]; then
-		printf "^c#56B6C2^  $LIGHT%%"
-	elif [[ ("$LIGHT" -ge "50") && ("$LIGHT" -le "75") ]]; then
-		printf "^c#56B6C2^  $LIGHT%%"
-	elif [[ ("$LIGHT" -ge "75") && ("$LIGHT" -le "100") ]]; then
-		printf "^c#56B6C2^  $LIGHT%%"
-	fi
-}
-
 ## Main
 while true; do
-  [ "$interval" == 0 ] || [ $(("$interval" % 3600)) == 0 ] && updates=$(updates)
-  interval=$((interval + 1))
-
-  sleep 1 && xsetroot -name "$(battery) $(brightness) $(cpu_info) $(memory) $(wlan) $(clock)"
+  sleep 1 && xsetroot -name "$(battery) $(wlan) $(clock)"
 done
