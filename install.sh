045d424a45511bbcf6cd4b4ee038bbf634fd505e
r3duc3 <itsreduce@gmail.com>
2022-12-22 23:02:40 +0700

    sudo

--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/bash
 
-if [ $USER != 'root' ]; then
-	echo 'run as root please';
-	exit 0;
-fi
-
 echo 'installing required packages (maybe)'
 sudo pacman -S --needed --disable-download-timeout libx11 libxinerama libxft freetype2 pcmanfm geany rofi dunst maim xclip viewnior feh ksuperkey betterlockscreen xfce4-power-manager xorg-xsetroot xorg-xbacklight gpick ffmpeg xorg-xrandr dunst pamixer pulseaudio pulseaudio-alsa firefox xfce-polkit xfce4-clipman-plugin gpick
 
 echo 'install some package'
-mkdir -p /usr/share/archcraft
-cp -rf archcraft/shared /usr/share/archcraft/dwm
-chmod +x /usr/share/archcraft/dwm/bin/*
-chmod +x /usr/share/archcraft/dwm/rofi/bin/*
+sudo mkdir -p /usr/share/archcraft
+sudo cp -rf archcraft/shared /usr/share/archcraft/dwm
+sudo chmod +x /usr/share/archcraft/dwm/bin/*
+sudo chmod +x /usr/share/archcraft/dwm/rofi/bin/*
 
 git clone https://github.com/archcraft-os/archcraft-packages.git
 
@@ -32,10 +27,10 @@ patch --forward --strip=1 --input="st-mouse-scroll_1.patch"
 patch --forward --strip=1 --input="st-mouse-scroll_2.patch"
 patch --forward --strip=1 --input="st-mouse-scroll_3.patch"
 make X11INC=/usr/include/X11 X11LIB=/usr/lib/X11
-make PREFIX=/usr install
+sudo make PREFIX=/usr install
 cd ..
 
 echo 'install dwm'
 cd archcraft-dwm/source
 make X11INC=/usr/include/X11 X11LIB=/usr/lib/X11 FREETYPEINC=/usr/include/freetype2
-make PREFIX=/usr install
+sudo make PREFIX=/usr install
